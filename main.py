54eb4625b4dc81b241e010decbe229b9594d3687
migueldavidve <michaelvera1613@gmail.com>
2025-02-04 14:42:23 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+numero = int(input("introduce un numero"))
+numero2 =int(input("introduce otro numero para sumar"))
+
+resultado = numero + numero2
+
+print(f"su resultado es {resultado} ")
+
+
+
+
